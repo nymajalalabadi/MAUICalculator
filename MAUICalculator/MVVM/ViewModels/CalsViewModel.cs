5735aa9bef5952368a2d9eb864a279a5d1f301a9
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-07-11 20:51:51 -0600

    Installed PropertyChanged.Fody Package

---
+++ MAUICalculator/MVVM/ViewModels/CalsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using PropertyChanged;
+using System.Windows.Input;
+
+namespace MAUICalculator.MVVM.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CalsViewModel
+    {
+        public string Formula { get; set; }
+
+        public string Result { get; set; } = "0";
+
+        public ICommand OperationCommand => new Command((number) =>
+        {
+            Formula += number;
+        });
+    }
+}
