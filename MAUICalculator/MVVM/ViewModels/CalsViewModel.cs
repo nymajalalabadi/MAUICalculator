986f311a7ede3a9cf00ed0accfa931890bd776e2
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-07-13 10:15:58 -0600

    Completed App Mobile

--- MAUICalculator/MVVM/ViewModels/CalsViewModel.cs
+++ MAUICalculator/MVVM/ViewModels/CalsViewModel.cs
@@ -1,4 +1,5 @@
-﻿using PropertyChanged;
+﻿using Dangl.Calculator;
+using PropertyChanged;
 using System.Windows.Input;
 
 namespace MAUICalculator.MVVM.ViewModels
@@ -14,5 +15,33 @@ namespace MAUICalculator.MVVM.ViewModels
         {
             Formula += number;
         });
+
+        public ICommand ResetCommand => new Command(() =>
+        {
+            Formula = string.Empty;
+            Result = "0";
+        });
+
+        public ICommand BackspaceCommand => new Command(() =>
+        {
+            if (Formula.Length > 0)
+            {
+                Formula = Formula.Substring(0, Formula.Length - 1);
+            }
+        });
+
+        public ICommand CalculateCommand => new Command(() =>
+        {
+            if (Formula.Length == 0)
+            {
+                Result = "0";
+                return;
+            }
+
+            var calculator = Calculator.Calculate(Formula);
+
+            Result = calculator.Result.ToString();
+        });
+
     }
 }
